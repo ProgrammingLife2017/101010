858c58d03e8203500788fb4bb39591e6743712d8
Jochem Tiessen <J.J.Tiessen@student.tudelft.nl>
2017-04-26 15:08:09 +0200

    Singleton
    Make Parser class singleton.

--- src/main/java/Parsing/Parser.java
+++ src/main/java/Parsing/Parser.java
@@ -1,7 +1,21 @@
 package Parsing;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 /**
  * Created by Jochem on 26-4-2017.
  */
 public class Parser {
+    private static Parser instance = null;
+
+    protected Parser() {}
+
+    public static Parser getInstance() {
+        if (instance == null) {
+            instance = new Parser();
+        }
+        return instance;
+    }
 }

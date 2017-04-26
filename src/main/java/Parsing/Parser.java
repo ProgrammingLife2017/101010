9a5daf67f1396ef6669fa2ed367809efa02c90a2
Jochem Tiessen <J.J.Tiessen@student.tudelft.nl>
2017-04-26 16:26:07 +0200

    Segments method
    Add segments method which parses segments from the test files. Only works with the test.gfa file added in resources.

--- src/main/java/Parsing/Parser.java
+++ src/main/java/Parsing/Parser.java
@@ -1,8 +1,9 @@
 package Parsing;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.util.TreeMap;
 
 /**
  * Created by Jochem on 26-4-2017.
@@ -17,5 +18,27 @@ public class Parser {
             instance = new Parser();
         }
         return instance;
+    }
+
+    public TreeMap segments(String filename) {
+        TreeMap result = new TreeMap();
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(filename)));
+            String line = in.readLine();
+            String key;
+            while (line != null) {
+                if (line.startsWith("S")) {
+                    line = line.substring(2);
+                    key = line.substring(0, line.indexOf("\t"));
+                    line = line.substring(line.indexOf("\t"));
+                    //TODO fix for actual file
+                    result.put(key, line);
+                }
+                line = in.readLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }

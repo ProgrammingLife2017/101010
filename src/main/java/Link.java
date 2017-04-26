26c1164059590d473e22480caa9dc9eacc68b1b3
Jochem Tiessen <J.J.Tiessen@student.tudelft.nl>
2017-04-26 13:39:37 +0200

    Add Link and Node classes
    Link and Node classes for representing data which will be parsed into these classes.

---
+++ src/main/java/Link.java
@@ -0,0 +1,38 @@
+/**
+ * Created by Jochem on 26-4-2017.
+ */
+public class Link {
+    Node first;
+    Node second;
+    String cigar;
+
+    public Link(Node first, Node second, String cigar) {
+        this.first = first;
+        this.second = second;
+        this.cigar = cigar;
+    }
+
+    public Node getFirst() {
+        return first;
+    }
+
+    public void setFirst(Node first) {
+        this.first = first;
+    }
+
+    public Node getSecond() {
+        return second;
+    }
+
+    public void setSecond(Node second) {
+        this.second = second;
+    }
+
+    public String getCigar() {
+        return cigar;
+    }
+
+    public void setCigar(String cigar) {
+        this.cigar = cigar;
+    }
+}
---
+++ src/main/java/Node.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Jochem on 26-4-2017.
+ */
+public class Node {
+    String segment;
+
+    public Node(String segment) {
+        this.segment = segment;
+    }
+
+    public String getSegment() {
+        return segment;
+    }
+
+    public void setSegment(String segment) {
+        this.segment = segment;
+    }
+}

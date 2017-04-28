5aaf7ab6ec95b5a132af7f59b7c68cdcfb14041f
Jochem Tiessen <J.J.Tiessen@student.tudelft.nl>
2017-04-28 18:31:24 +0200

    ReverseComplement
    Adds method to Node which sets the segment of that Node to the reverseComplement of it.

--- src/main/java/Parsing/Node.java
+++ src/main/java/Parsing/Node.java
@@ -27,4 +27,32 @@ public class Node {
         }
         return false;
     }
+
+    public void reverseComplement() {
+        char[] chars = this.getSegment().toCharArray();
+        int length = chars.length;
+        String result = "";
+        for (int i = 0; i < length; i++) {
+            char c = chars[i];
+            switch (c) {
+                case 'A':
+                    c = 'T';
+                    break;
+                case 'T':
+                    c = 'A';
+                    break;
+                case 'C':
+                    c = 'G';
+                    break;
+                case 'G':
+                    c = 'C';
+                    break;
+                default:
+                    c = c;
+                    break;
+            }
+            result = c + result;
+        }
+        this.setSegment(result);
+    }
 }

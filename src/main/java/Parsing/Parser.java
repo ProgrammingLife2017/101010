43288f39ad25eb01e040263c8768b1d049e5ea80
Jochem Tiessen <J.J.Tiessen@student.tudelft.nl>
2017-04-29 16:23:56 +0200

    parse now returns a DirectedGraph

--- src/main/java/Parsing/Parser.java
+++ src/main/java/Parsing/Parser.java
@@ -2,7 +2,8 @@ package Parsing;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.TreeMap;
+
+import org.jgrapht.graph.DefaultDirectedGraph;
 
 /**
  * Created by Jochem on 26-4-2017.
@@ -19,22 +20,22 @@ public class Parser {
         return instance;
     }
 
-    public TreeMap segments(String filename) {
-        TreeMap result = new TreeMap();
-//        Link
+    public DefaultDirectedGraph<Node, Link> parse(String filename) {
+        DefaultDirectedGraph<Node, Link> graph = new DefaultDirectedGraph<Node, Link>(Link.class);
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream(filename)));
             String line = in.readLine();
-            String key;
             while (line != null) {
                 if (line.startsWith("S")) {
+                    Node temp = new Node("", 0);
                     line = line.substring(2);
-                    key = line.substring(0, line.indexOf("\t"));
+                    temp.setId(Integer.parseInt(line.substring(0, line.indexOf("\t"))));
                     line = line.substring(line.indexOf("\t") + 1);
                     if (line.indexOf("\t") != -1) {
                         line = line.substring(0, line.indexOf("\t"));
                     }
-                    result.put(Integer.parseInt(key), new Node(line));
+                    temp.setSegment(line);
+                    graph.addVertex(temp);
                 } else if (line.startsWith("L")) {
                     line = line.substring(2);
                     System.out.println(line);
@@ -44,15 +45,6 @@ public class Parser {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return result;
-    }
-
-    public static void main(String args[]) {
-        Parser parser = Parser.getInstance();
-        TreeMap map = parser.segments("/test.gfa");
-        Node first = (Node) map.get(map.firstKey());
-        System.out.println(first.getSegment());
-        first.reverseComplement();
-        System.out.println(first.getSegment());
+        return graph;
     }
 }

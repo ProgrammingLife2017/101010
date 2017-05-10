4605245773660005fb245f373edb3fe0ec1d4f08
DexterQvL <dq_vanleeuwen@hotmail.com>
2017-05-10 11:38:12 +0200

    Added getNodes and addNode methods to get nodes from the database and insert nodes into the database.

--- src/main/java/Datastructure/NodeMap.java
+++ src/main/java/Datastructure/NodeMap.java
@@ -10,7 +10,6 @@ import java.sql.*;
 public class NodeMap {
     HashIntObjMap<int[]> map;
     String dbLoc;
-    Connection con;
 
     public NodeMap() {
         this.map = HashIntObjMaps.newUpdatableMap();
@@ -35,17 +34,87 @@ public class NodeMap {
      *      node -  PRIMARY KEY:    id (INT)
      *              OTHER:          segment (CLOB, NOT NULL)
      *                              coordinate (INT, NOT NULL)
-     *      edges - PRIMARY KEY:    from (INT, FOREIGN KEY node(id))
-     *                              to (INT, FOREIGN KEY node(id))
+     *      edges - PRIMARY KEY:    from_id (INT, FOREIGN KEY node(id))
+     *                              to_id (INT)
      */
     private void dbInit() {
         try {
             Class.forName("org.h2.Driver");
-            con = DriverManager.getConnection(dbLoc);
+            Connection con = DriverManager.getConnection(dbLoc);
             con.prepareStatement("DROP TABLE IF EXISTS node").execute();
             con.prepareStatement("DROP TABLE IF EXISTS edges").execute();
             con.prepareStatement("CREATE TABLE node(id INT PRIMARY KEY, coordinate INT NOT NULL, segment CLOB NOT NULL)").execute();
-            con.prepareStatement("CREATE TABLE edges(from_id INT PRIMARY KEY, FOREIGN KEY (from_id) REFERENCES node(id), to_id INT NOT NULL, FOREIGN KEY (to_id) REFERENCES node(id))").execute();
+            con.prepareStatement("CREATE TABLE edges(from_id INT PRIMARY KEY, FOREIGN KEY (from_id) REFERENCES node(id), to_id INT PRIMARY KEY)").execute();
+            con.close();
+        }
+        catch(ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns a ResultSet of all nodes at a certain coordinate.
+     * @param coordinate - the coordinate of the requested nodes.
+     * @return A resultset containing all nodes at the given coordinate.
+     */
+    private ResultSet getNodes(int coordinate) {
+        try {
+            Class.forName("org.h2.Driver");
+            Connection con = DriverManager.getConnection(dbLoc);
+            PreparedStatement stmt = con.prepareStatement("SELECT * FROM NODE WHERE coordinate = ?");
+            stmt.setInt(1, coordinate);
+            ResultSet res = stmt.executeQuery();
+            con.close();
+            return res;
+        }
+        catch(ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Inserts a node and edges into the database based on the provided parameters.
+     * @param id - the id of the node.
+     * @param coordinate - the coordinate of the node.
+     * @param segment - the segment of the node.
+     * @param destinationIDs - the ids of the nodes the outgoing edges of this node go to.
+     */
+    public void addNode(int id, int coordinate, String segment, int[] destinationIDs) {
+        try {
+            String iNode =
+                    "INSERT INTO NODE" +
+                    "VALUES (?, ?, ?)";
+            String iEdges =
+                    "INSERT INTO EDGES" +
+                    "VALUES (?, ?)";
+
+            Class.forName("org.h2.Driver");
+            Connection con = DriverManager.getConnection(dbLoc);
+
+            PreparedStatement insertNode = con.prepareStatement(iNode);
+            insertNode.setInt(1, id);
+            insertNode.setInt(2, coordinate);
+            insertNode.setString(3, segment);
+            insertNode.execute();
+
+            PreparedStatement insertEdges = con.prepareStatement(iEdges);
+            insertEdges.setInt(1, id);
+            for(int i = 0; i < destinationIDs.length; i++) {
+                insertEdges.setInt(2, destinationIDs[i]);
+                insertEdges.execute();
+            }
+
             con.close();
         }
         catch(ClassNotFoundException e) {

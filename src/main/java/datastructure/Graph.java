818b0d069f053958cc3e9fbdecfbe7f1d34a3829
Jochem Tiessen <J.J.Tiessen@student.tudelft.nl>
2017-05-15 11:29:53 +0200

    Change adjacent to nodes
    Now saving nodes instead of a list of node ids.

--- src/main/java/datastructure/Graph.java
+++ src/main/java/datastructure/Graph.java
@@ -1,44 +1,22 @@
 package datastructure;
 
-import com.sun.xml.internal.bind.v2.TODO;
-
 import java.util.*;
 
 /**
  * Created by 101010 on 14-5-2017.
  */
 public class Graph {
-    private HashMap<Integer, LinkedList<Integer>> adjacent;
+    private HashMap<Integer, Node> nodes;
 
-    public Graph(int v) {
-        adjacent = new HashMap<>();
+    public Graph() {
+        nodes = new HashMap<>();
     }
 
-    public void addEdge(int c, int n) {
-        LinkedList<Integer> current = adjacent.get(c);
-        if (current.equals(null)) {
-            current = new LinkedList<Integer>();
-        }
-        current.add(n);
+    public void addNode(Node node) {
+        nodes.put(Integer.parseInt(node.getId()), node);
     }
 
-//    public void sortUtil(int c, boolean[] visited) {
-//        visited[c] = true;
-//        int n;
-//
-//        Iterator<Integer> it = adjacent.get(c).iterator();
-//        while(it.hasNext()) {
-//            n = it.next();
-//            if (!visited[n]) {
-//                sortUtil(n, visited);
-//            }
-//        }
-//    }
-
-    public void drawGraph() {
-        int nodeAmount = adjacent.size();
-        Set<Integer> visited = new HashSet<>();
-
-
+    public HashMap<Integer, Node> getNodes() {
+        return this.nodes;
     }
 }

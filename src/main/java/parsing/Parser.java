2871c7e651eab01542ef8ff37603a97f2a8354e5
DexterQvL <dq_vanleeuwen@hotmail.com>
2017-05-19 09:28:55 +0200

    Reversed full import to individual imports.

--- src/main/java/parsing/Parser.java
+++ src/main/java/parsing/Parser.java
@@ -4,7 +4,17 @@ import datastructure.Node;
 import datastructure.NodeGraph;
 import datastructure.SegmentDB;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayDeque;
 import java.util.Queue;
 

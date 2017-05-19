cbd3876f7c29ce991da69ea5a4e1c8611f239d49
DexterQvL <dq_vanleeuwen@hotmail.com>
2017-05-19 09:27:05 +0200

    Split parser into two methods, one for cold load and one for warm load. Also removed commented or otherwise redundant code.

--- src/main/java/parsing/Parser.java
+++ src/main/java/parsing/Parser.java
@@ -4,21 +4,8 @@ import datastructure.Node;
 import datastructure.NodeGraph;
 import datastructure.SegmentDB;
 
+import java.io.*;
 import java.util.ArrayDeque;
-import java.util.HashSet;
-import javafx.util.Pair;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.FileReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Queue;
 
 
@@ -54,111 +41,74 @@ public class Parser {
      */
     public NodeGraph parse(final File file) {
         NodeGraph graph = new NodeGraph();
-        return parse(file, graph);
+
+        String cacheName = file.getAbsolutePath().substring(0, file.getAbsolutePath().length() - 4);
+
+        File cache = new File(cacheName + ".txt");
+
+        if (cache.exists()) {
+            return parseCache(graph, cache);
+        }
+
+        return parse(file, cache, graph);
     }
 
     /**
      * Parses a .gfa file to a graph.
      * @param file The name of the target .gfa file.
      * @param graph The graph the data gets put into.
+     * @param newCache The file that the cache will be written to.
      * @return The graph created from the .gfa file.
      */
-    public NodeGraph parse(final File file, NodeGraph graph) {
+    public NodeGraph parse(final File file, File newCache, NodeGraph graph) {
 
         try {
             BufferedReader in = new BufferedReader(
                     new FileReader(file));
             String line = in.readLine();
-            boolean newCache = true;
             line = line.substring(line.indexOf("\t") + 1);
             line = line.replaceAll(":", "");
-            String cacheName = file.getName().substring(0, file.getName().length() - 4);
+
             String absoluteFilePath = file.getAbsolutePath().substring(0, file.getAbsolutePath().length() - 4);
 
             graph.setSegmentDB(new SegmentDB(absoluteFilePath + "Segments.txt"));
             File segments = new File(absoluteFilePath + "Segments.txt");
 
-            System.out.println(absoluteFilePath + "Segments.txt");
+            segments.createNewFile();
 
-            if (!segments.exists()) {
-                segments.createNewFile();
-            }
+            BufferedWriter out = new BufferedWriter(new FileWriter(segments));
 
-            BufferedWriter out = new BufferedWriter(new FileWriter(segments, true));
-            File newFile = new File(absoluteFilePath + ".txt");
-            if (newFile.exists()) {
-                newCache = false;
-                BufferedReader in2 = new BufferedReader(new InputStreamReader(new FileInputStream(newFile)));
-                line = in2.readLine();
-                int graphSize = Integer.parseInt(line);
-                graph.getNodes().ensureCapacity(graphSize);
-                for (int i = 0; i < graphSize; i++) {
-                    line = in2.readLine();
-                    int length = Integer.parseInt(line);
-                    line = in2.readLine();
-                    int x = Integer.parseInt(line);
-                    line = in2.readLine();
-                    int y = Integer.parseInt(line);
-//                    int inLength = Integer.parseInt(line);
-//                    int[] ingoing = new int[inLength];
-//                    for (int j = 0; j < inLength; j++) {
-//                        line = in2.readLine();
-//                        ingoing[j] = Integer.parseInt(line);
-//                    }
-                    line = in2.readLine();
-                    int outLength = Integer.parseInt(line);
-                    int[] outgoing = new int[outLength];
-                    for (int j = 0; j < outLength; j++) {
-                        line = in2.readLine();
-                        outgoing[j] = Integer.parseInt(line);
-                    }
-                    Node temp = new Node(length, outgoing, new int[0]);
-                    temp.setX(x);
-                    temp.setY(y);
-                    graph.addNodeCache(i, temp);
-                }
-            }
-            BufferedReader br2 = new BufferedReader(new FileReader(file));
-            String lastline = br2.readLine();
-            String currentLine = "";
-            while ((currentLine = br2.readLine()) != null)
-                lastline = currentLine;
-            lastline = lastline.substring(lastline.indexOf("\t") + 1);
-            int noOfNodes = Integer.parseInt(lastline.substring(0, lastline.indexOf("\t"))) - 1;
-            graph.getNodes().ensureCapacity(noOfNodes);
-            if (newCache) {
-                while (line != null) {
-                    if (line.startsWith("S")) {
-                        int id;
-                        String segment;
+            while (line != null) {
+                if (line.startsWith("S")) {
+                    int id;
+                    String segment;
+                    line = line.substring(line.indexOf("\t") + 1);
+                    id = Integer.parseInt(line.substring(0, line.indexOf("\t"))) - 1;
+                    line = line.substring(line.indexOf("\t") + 1);
+                    segment = line.substring(0, line.indexOf("\t"));
+                    graph.addNode(id, new Node(segment.length(), new int[0], new int[0]));
+                    out.write(segment + "\n");
+                    out.flush();
+                    line = in.readLine();
+                    while (line != null && line.startsWith("L")) {
+                        int from;
+                        int to;
                         line = line.substring(line.indexOf("\t") + 1);
-                        id = Integer.parseInt(line.substring(0, line.indexOf("\t"))) - 1;
-                        line = line.substring(line.indexOf("\t") + 1);
-                        segment = line.substring(0, line.indexOf("\t"));
-                        graph.addNode(id, new Node(segment.length(), new int[0], new int[0]));
-                        out.write(segment + "\n");
-                        out.flush();
-                        line = in.readLine();
-                        while (line != null && line.startsWith("L")) {
-                            int from;
-                            int to;
-                            line = line.substring(line.indexOf("\t") + 1);
-                            from = Integer.parseInt(line.substring(0, line.indexOf("\t"))) - 1;
-                            line = line.substring(line.indexOf("+") + 2);
-                            to = Integer.parseInt(line.substring(0, line.indexOf("\t"))) - 1;
-                            graph.addEdge(from, to);
-                            line = in.readLine();
-                        }
-                    } else {
+                        from = Integer.parseInt(line.substring(0, line.indexOf("\t"))) - 1;
+                        line = line.substring(line.indexOf("+") + 2);
+                        to = Integer.parseInt(line.substring(0, line.indexOf("\t"))) - 1;
+                        graph.addEdge(from, to);
                         line = in.readLine();
                     }
+                } else {
+                    line = in.readLine();
                 }
-                for (int i = 0; i < graph.getSize(); i++) {
-                    graph.getNode(i).setInDegree(graph.getNode(i).getIncomingEdges().length);
-                }
-                kahnAlgorithm(graph);
-                createCache(absoluteFilePath, graph);
             }
+            for (int i = 0; i < graph.getSize(); i++) {
+                graph.getNode(i).setInDegree(graph.getNode(i).getIncomingEdges().length);
+            }
+            kahnAlgorithm(graph);
+            createCache(absoluteFilePath, graph);
         } catch (FileNotFoundException e) {
             System.out.println("Wrong file Destination");
             e.printStackTrace();
@@ -168,6 +118,39 @@ public class Parser {
         }
 
 
+        return graph;
+    }
+
+
+    public NodeGraph parseCache(NodeGraph graph, File cache) {
+        try {
+            BufferedReader in2 = new BufferedReader(new InputStreamReader(new FileInputStream(cache)));
+            String line = in2.readLine();
+            int graphSize = Integer.parseInt(line);
+            graph.getNodes().ensureCapacity(graphSize);
+            for (int i = 0; i < graphSize; i++) {
+                line = in2.readLine();
+                int length = Integer.parseInt(line);
+                line = in2.readLine();
+                int x = Integer.parseInt(line);
+                line = in2.readLine();
+                int y = Integer.parseInt(line);
+                line = in2.readLine();
+                int outLength = Integer.parseInt(line);
+                int[] outgoing = new int[outLength];
+                for (int j = 0; j < outLength; j++) {
+                    line = in2.readLine();
+                    outgoing[j] = Integer.parseInt(line);
+                }
+                Node temp = new Node(length, outgoing, new int[0]);
+                temp.setX(x);
+                temp.setY(y);
+                graph.addNodeCache(i, temp);
+            }
+        } catch (IOException e) {
+            System.out.println("Error while reading cache");
+            e.printStackTrace();
+        }
         return graph;
     }
 
@@ -224,14 +207,6 @@ public class Parser {
                 writer.newLine();
                 writer.write("" + y);
                 writer.newLine();
-//                int[] tempList = temp.getIncomingEdges();
-//                size = tempList.length;
-//                writer.write("" + size);
-//                writer.newLine();
-//                for (int j = 0; j < size; j++) {
-//                    writer.write("" + tempList[j]);
-//                    writer.newLine();
-//                }
                 int[] tempList = temp.getOutgoingEdges();
                 size = tempList.length;
                 writer.write("" + size);

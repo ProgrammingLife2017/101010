2d317c372b58ad072bde994d9408ce0fc5dc3402
Michael Tran <m.tran@student.tudelft.nl>
2017-05-26 12:42:08 +0200

    Refactoring of Backlog.

--- src/main/java/screens/Backlog.java
+++ src/main/java/screens/Backlog.java
@@ -8,32 +8,43 @@ import javafx.stage.Stage;
 /**
  * Implementation of the backlog screen to keep track of actions that are performed by the application.
  */
-public final class Backlog extends Stage {
+public final class Backlog {
     /**
      * Area to write content of the logger.
      */
     private final TextArea textArea;
 
+    private final Stage stage;
+
+    private static double ANCHOR_OFFSET = 10d;
+
+    private static double PREFFERED_HEIGHT = 300d;
+
+    private static double PREFFERED_WIDTH = 600d;
     /**
      * Backlog window constructor.
      */
     /* package */ Backlog() {
         textArea = new TextArea();
+        stage = new Stage();
+    }
+
+    public void setup() {
         textArea.setEditable(false);
         textArea.setVisible(true);
         textArea.textProperty().addListener(
                 (observable, oldValue, newValue) -> textArea.setScrollTop(Double.MAX_VALUE)
         );
-        this.setTitle("Backlog");
+        stage.setTitle("Backlog");
         AnchorPane root = new AnchorPane();
-        root.setPrefSize(600.0, 300.0);
-        AnchorPane.setTopAnchor(textArea, 10.0);
-        AnchorPane.setLeftAnchor(textArea, 10.0);
-        AnchorPane.setRightAnchor(textArea, 10.0);
-        AnchorPane.setBottomAnchor(textArea, 10.0);
+        root.setPrefSize(PREFFERED_WIDTH, PREFFERED_HEIGHT);
+        AnchorPane.setTopAnchor(textArea, ANCHOR_OFFSET);
+        AnchorPane.setLeftAnchor(textArea, ANCHOR_OFFSET);
+        AnchorPane.setRightAnchor(textArea, ANCHOR_OFFSET);
+        AnchorPane.setBottomAnchor(textArea, ANCHOR_OFFSET);
         root.getChildren().add(textArea);
         Scene scene = new Scene(root);
-        this.setScene(scene);
+        stage.setScene(scene);
     }
 
     /**
--- src/test/java/screens/BacklogTest.java
+++ src/test/java/screens/BacklogTest.java
@@ -2,21 +2,34 @@ package screens;
 
 import javafx.scene.control.TextArea;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.mock;
 
 /**
  * Test class for the backlog window.
  */
+@RunWith(PowerMockRunner.class)
 public class BacklogTest {
     private Backlog log;
     private TextArea area;
 
     @Before
-    public void setup() {
-        log = new Backlog();
+    public void setup() throws Exception{
+        log = Whitebox.invokeConstructor(Backlog.class);
         area = mock(TextArea.class);
         Whitebox.setInternalState(log, "textArea", area);
+    }
+
+    @Test
+    public void testPrintContent() {
+        String testString = "hello world";
+        log.printContent(testString);
+        verify(area, times(1)).appendText(testString);
     }
 }

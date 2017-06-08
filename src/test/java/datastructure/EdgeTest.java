090bd1d6befbd1a4fe0fdf6dc6c8b2c1511d3564
tvheems <T.J.vanHeems@student.tudelft.nl>
2017-06-08 13:45:41 +0200

    deleted star import

--- src/test/java/datastructure/EdgeTest.java
+++ src/test/java/datastructure/EdgeTest.java
@@ -4,7 +4,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 
 /**
  * Created by 101010.
